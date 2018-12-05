221e4a1e1faa5e89ca33b5ab7f690b4043e58bb3
CoreyCole <coreyleoc@gmail.com>
2018-12-05 00:32:51 -0800

    feat(5): use property and setter decorators

--- 05-Circle/circle.py
+++ 05-Circle/circle.py
@@ -2,21 +2,43 @@ from math import pi
 
 
 class Circle:
+    """Circle with radius, diameter and area."""
     radius: float
     diameter: float
+    area: float
 
     def __init__(self, r: float = 1.0):
         if r < 0:
             raise ValueError('Radius cannot be negative')
         self.radius = r
-        self.diameter = r * 2
-        self.area = pi * self.radius * self.radius
+
+    @property
+    def radius(self):
+        return self._radius
+
+    @radius.setter
+    def radius(self, radius):
+        if radius < 0:
+            raise ValueError('Radius cannot be negative')
+        self._radius = radius
+
+    @property
+    def diameter(self):
+        return self.radius * 2
+
+    @diameter.setter
+    def diameter(self, diameter):
+        self.radius = diameter / 2
+
+    @property
+    def area(self):
+        return pi * self.radius ** 2
 
     def __eq__(self, other) -> bool:
         return self.radius is other.radius
 
     def __str__(self) -> str:
-        return 'Circle(' + str(self.radius) + ')'
+        return f'Circle({self.radius})'
 
     def __repr__(self) -> str:
-        return 'Circle(' + str(self.radius) + ')'
+        return f'Circle({self.radius})'
